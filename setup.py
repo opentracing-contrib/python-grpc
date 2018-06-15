c58f3560662a4c87dfdec48b9b7cafb4ca2ad4be
Ryan Burn <ryan.burn@gmail.com>
2018-06-15 15:20:48 -0400

    Set up continuous deployment.

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import sys
 import tempfile
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
+VERSION='1.1.2'
 
 def readme():
   """Use `pandoc` to convert `README.md` into a `README.rst` file."""
@@ -18,19 +20,39 @@ def readme():
   return ''
 
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setup(
     name='grpcio-opentracing',
-    version='1.1.2',
+    version=VERSION,
     description='Python OpenTracing Extensions for gRPC',
     long_description=readme(),
     author='LightStep',
     license='',
     install_requires=['opentracing>=1.2.2', 'grpcio>=1.1.3', 'six>=1.10'],
-    setup_requires=['pytest-runner'],
+    setup_requires=['pytest-runner', 'pandoc'],
     tests_require=['pytest', 'future'],
     keywords=['opentracing'],
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
     ],
-    packages=find_packages(exclude=['docs*', 'tests*', 'examples*']))
+    packages=find_packages(exclude=['docs*', 'tests*', 'examples*']),
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)

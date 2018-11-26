22cf5e24b5b19d693fdf5a55e756211e5bccdae7
zhyon404 <zhyong4@gmail.com>
2018-11-27 00:49:14 +0800

    fix ci

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     license='',
     install_requires=['opentracing>=1.2.2', 'grpcio>=1.8.1', 'six>=1.10'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'future'],
+    tests_require=['pytest==3.8.2', 'future'],
     keywords=['opentracing'],
     classifiers=[
         'Operating System :: OS Independent',

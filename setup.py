a6a308594ac331973bd835869f5430cd32c3c03e
zhyon404 <zhyong4@gmail.com>
2019-01-21 11:59:01 +0800

    Change minimal requirement of grpcio to 1.8.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     long_description=readme(),
     author='LightStep',
     license='',
-    install_requires=['opentracing>=1.2.2', 'grpcio>=1.8.1', 'six>=1.10'],
+    install_requires=['opentracing>=1.2.2', 'grpcio>=1.8.0', 'six>=1.10'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==3.8.2', 'future'],
     keywords=['opentracing'],

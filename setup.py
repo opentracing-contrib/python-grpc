5547b4b29c665580df7303b219454713f7f8e26e
Guilherme Souza <guilherme@souza.tech>
2019-01-29 18:51:00 +0100

    Adding license information to setup.py

--- setup.py
+++ setup.py
@@ -40,13 +40,14 @@ setup(
     description='Python OpenTracing Extensions for gRPC',
     long_description=readme(),
     author='LightStep',
-    license='',
+    license='Apache',
     install_requires=['opentracing>=1.2.2', 'grpcio>=1.1.3', 'six>=1.10'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'future'],
     keywords=['opentracing'],
     classifiers=[
         'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6"
